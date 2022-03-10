13fb8ad5802bd6de29928bcd0961dac318807ff7
himla-Lois <89983005+himla-Lois@users.noreply.github.com>
2022-03-10 17:52:57 +0100

    Update EventQueue.java

--- lab/src/lab5/events/EventQueue.java
+++ lab/src/lab5/events/EventQueue.java
@@ -6,6 +6,7 @@ package lab5.events;
  */
 
 public class EventQueue {
+    LinkedList<Event> linkedList = new LinkedList<Event>();
     Node head;
 
     /**
@@ -58,6 +59,26 @@ public class EventQueue {
     public void removeFirst(){
         head = head.next;
         return;
+    }
+    
+    /**
+     * Removes first node in the LinkedList.
+     */
+    public void removeFirst(){
+        head = head.next;
+        return;
+    }
+
+    /**
+     * Gets next event in linkedList
+     */
+    public Event getFirstEvent(){ 
+        return linkedList.getFirst();
+    }
+
+    public boolean hasEvent(){
+        return false;
+        //TODO: Checks if the linked list has a next event. 
     }
 
 }
