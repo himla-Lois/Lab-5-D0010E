de9304c17a8f0129cdee1abf2b3aad33cc41aec9
strandafredde <75703920+strandafredde@users.noreply.github.com>
2022-03-11 22:52:50 +0100

    Update State.java

--- lab/src/lab5/state/State.java
+++ lab/src/lab5/state/State.java
@@ -1,13 +1,25 @@
 package lab5.state;
-
 import java.util.Observable;
 
 public class State extends Observable {
-    private int currentState;
-    private boolean eBrake;
+    private int currentState = 0;
+    private boolean eBrake = false;
 
     // TODO: should be able to keep track of runtime.
     //  And should call the observer every time an EVENT has been triggered.
-
+    
+    public int addTime(int time) {
+    	currentState += time;
+		return currentState;
+    }
+    
+    
+    public void stopSim(){
+    	eBrake = true;
+    }
+	public void callObservers(){
+		setChanged();
+		notifyObservers();
+	}
 }
 
