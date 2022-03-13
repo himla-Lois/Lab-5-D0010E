9fa1dbd3d7fa3c1c117121968a80c382270ac8fe
Jay <89936885+JCG333@users.noreply.github.com>
2022-03-13 14:30:49 +0100

    Updated Start event
    created the general stuff to be inherited by open event.

--- lab/src/lab5/events/StartEvent.java
+++ lab/src/lab5/events/StartEvent.java
@@ -1,4 +1,20 @@
 package lab5.events;
 
-public class StartEvent extends Event{
+import lab5.state.State;
+
+/**
+ * This class is a general class for the starting event of the simulation
+ */
+public abstract class StartEvent extends Event{
+
+    /**
+     *
+     * @param state the state of the simulation (in our case: ShoppingState).
+     * @param queue The queue at which events are added to.
+     * @param time  the time at which this should be preformed
+     */
+    public StartEvent(State state, EventQueue queue, double time){
+        super(state, queue, time);
+    }
 }
+
