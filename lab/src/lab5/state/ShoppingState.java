ba0cd9abd1285e86f1a23c04240c310acd570d34
himla-Lois <89983005+himla-Lois@users.noreply.github.com>
2022-03-13 18:22:19 +0100

    ShoppingState
    Has a few errors, trying to debug them but if yall find them. Change it.

--- lab/src/lab5/state/ShoppingState.java
+++ lab/src/lab5/state/ShoppingState.java
@@ -1,4 +1,7 @@
-package lab5.state;
+package state;
+
+import state.CustomerFactory;
+import state.FIFO;
 
 /**
  *  Special simulation representing a supermarket
@@ -10,15 +13,253 @@ public class ShoppingState extends State{
 
     FIFO customers = new FIFO();
 
-    private int N; // open cash-registers.
-    private int M; // maximum allowed customers in store.
-    private int S; // store closing-time(opens at 0).
-    private long F; // seed(start value for random-generators)
-    private int payingCustomer;
-    private int lostCustomer;
-    private float wastedCashierTime = 0;
-    private float queueTime;
+    final int N; // Number of registerd open cash
+    final int M; // maximum allowed customers
+    final double S; // closing-time (starts at 0)
+    final double lambda; // ankomsthastigheten för kunder
+    final double pickMin; // min picktime
+    final double pickMax; // max picktime
+    final double payMin; // min paytime
+    final double payMax; // max paytime
+    final long F; // seed/frö
 
-    // TODO
+     
+    private int customer;
+    private int payingCustomers;
+    private int lostCustomers;
+    private int queuedCustomers;
+
+    private int unoccupiedRegisters;
+
+    private double wastedCashierTime = 0;
+    private double queueTime;
+
+    private Customer eventCustomer;
+    private double lastPayTime;
+
+    private Object customerFactory;
+
+    private Object FIFO;
+
+    
+/** 
+     * The constructor for shoppingstate. Defines values based on arguments. 
+     *
+     * @param N The number of cash registers
+     * @param M Max customers in the store
+     * @param S Closing time
+     * @param lambda Arrival speed (Is used to generate random arrival times)
+     * @param pickMin Minimum time for a customer to pick their wares
+     * @param pickMax Maximum time for a customer to pick their wares
+     * @param payMin Minimum time for a customer to pay for their wares
+     * @param payMax Maximum time for a customer to pay for their wares
+     * @param F Seed used in random number generators
+     */
+    public ShoppingState(int N, int M, double S, double lambda, double pickMin, double pickMax, double payMin, double payMax, long F) {
+        super();
+        if(N <= 0 || M <= 0 || S <= 0 || lambda <= 0 || pickMin < 0 || pickMax <= 0 || payMin < 0 || payMax <= 0) {   // Checks parameters are invalid
+            throw new IllegalArgumentException("Illegal arguments in constructor");
+        }
+
+        this.customerFactory = new CustomerFactory();
+        this.FIFO   = new FIFO();
+        this.RandomArrival   = new RandomArrival(lambda, F);
+        this.shopRandom      = new ShopRandom(pickMin, pickMax, F);
+        this.payRandom       = new PayRandom(payMin, payMax, F);
+
+        this.N = N;
+        this.M = M;
+        this.S = S;
+        
+        this.lambda = lambda;
+        this.pickMin = pickMin;
+        this.pickMax = pickMax;
+        this.payMin = payMin;
+        this.payMax = payMax;
+
+        this.F = F;
+
+        this.customers       = 0;
+        this.payingCustomers = 0;
+        this.lostCustomers   = 0;
+        this.queuedCustomers = 0;
+
+        this.unoccupiedRegisters = N;
+
+        this.wastedCashierTime = 0.0d;
+        this.queueTime = 0.0d;
+
+        this.eventCustomer = null;
+        this.lastPayTime = 0.0d;
+        
+
+    Customer makeCustomer(){
+        return this.customerFactory.createCustomer();
+    }
+
+    String getQueueString() {
+            return this.FIFO.toString();
+    }
+
+
+    void addToQue(){
+        this.FIFO.add(Customer c);
+    }
+
+    Customer removeFromQue(){
+        this.FIFO.remove();
+    }
+
+    int getQlength(){
+        return this.FIFO.getSize();
+    }
+
+    double nextArrivalRandom() {
+        return this.arrivalRandom.getRandom();
+    }
+
+
+    double nextShopRandom() {
+        return this.shopRandom.getRandom();
+    }
+
+
+    double nextPayRandom() {
+        return this.payRandom.getRandom();
+    }
+
+    
+    /**
+     * Used by view to get the number of customers in the store
+     * @return returns the numbers of customers in the store 
+     */
+    int getNrOfCustomers() {
+        return this.customer;
+    }
+
+
+    void increaseCustomers() {
+        ++this.customer;
+    }
+
+
+    void decreaseCustomers() {
+        if(this.customer == 0) {
+            throw new RuntimeException("customers cannot be lower than 0");
+        }
+        --this.customer;
+    }
+
+    
+    int getPayingCustomers() {
+        return this.payingCustomers;
+    }
+
+
+    void increasePayingCustomers() {
+        ++this.payingCustomers;
+    }
+
+
+    /**
+     * Used by view and Optimize to get the customers that didn't pay
+     * @return returns the amount of lostCustomers 
+     */
+    public int getLostCustomers() {
+        return this.lostCustomers;
+    }
+
+
+    void increaseLostCustomer() {
+        ++this.lostCustomers;
+    }
+
+
+    double wastedCashierTime() {
+        return wastedCashierTime;
+    }
+
+
+    private void addToWastedCashierTime(double wastedCashierTime) {
+        if(wastedCashierTime < 0) {
+            throw new IllegalArgumentException("no negative time");
+        }
+        this.wastedCashierTime += wastedCashierTime;
+    }
+
+
+    double getQueueTime() {
+        return this.queueTime;
+    }
+
+
+    void addToQueueTime(double queueTime) {
+        if(queueTime < 0) {
+            throw new IllegalArgumentException("no negative time");
+        }
+        this.queueTime += queueTime;
+    }
+
+
+    int getUnoccupiedRegisters() { //IM HERE
+        return unoccupiedRegisters;
+    }
+
+
+    void increaseUnoccupiedRegisters() {
+        if(this.unoccupiedRegisters == this.N) {
+            throw new RuntimeException("unoccupiedRegisters cannot be greater than N");
+        }
+        ++this.unoccupiedRegisters;
+    }
+
+
+    void decUnoccupiedRegisters() {
+        if(this.unoccupiedRegisters == 0) {
+            throw new RuntimeException("unoccupiedRegisters cannot be lower than 0");
+        }
+        --this.unoccupiedRegisters;
+    }
+
+
+    int getQueuedCustomers() {
+        return queuedCustomers;
+    }
+
+
+    void incQueuedCustomers() {
+        ++this.queuedCustomers;
+    }
+
+
+    Customer getEventCustomer() {
+        return eventCustomer;
+    }
+
+
+    void setEventCustomer(Customer eventCustomer) {
+        this.eventCustomer = eventCustomer;
+    }
+
+    
+    void updateQAndRLogTime(double time) {
+        //Adds time to the two variables using the current time
+        this.addToUnusedCashloggedTime((time - this.getCurrentTime()) * this.getUnoccupiedRegisters());
+        
+        this.addToQueueTime((time - this.getCurrentTime()) * this.getQueueLength());
+    }
+
+
+    double getLastPayTime() {
+        return lastPayTime;
+    }
+
+
+    void setLastPayTime(double lastPayTime) {
+        if(lastPayTime < this.lastPayTime) {
+            throw new IllegalArgumentException("lastPayTime cannot be less than the previous time");
+        }
+        this.lastPayTime = lastPayTime;
+    }
 
 }
