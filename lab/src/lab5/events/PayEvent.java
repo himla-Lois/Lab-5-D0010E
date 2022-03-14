0d9bd2c3eddb9773a890fc0ac6661be9f25bd292
Jay <89936885+JCG333@users.noreply.github.com>
2022-03-14 14:45:21 +0100

    updated payEvent
    added all code for payEvent

--- lab/src/lab5/events/PayEvent.java
+++ lab/src/lab5/events/PayEvent.java
@@ -1,4 +1,30 @@
 package lab5.events;
 
+import lab5.state.Customer;
+import lab5.state.State;
+import lab5.state.ShoppingState;
+
+
 public class PayEvent extends Event{
+
+    public final int customerNumber;
+
+    public PayEvent(ShoppingState state, EventQueue queue, double time, int customerNumber) {
+        super(state, queue, time);
+        this.customerNumber = customerNumber;
+    }
+
+    public void effect(){
+
+        state.decreaseCustomers();
+        state.increaseUnoccupiedRegisters();
+        state.increaseFinishedCustomers();
+        if (state.getQlength() != 0){
+            state.removeFromQue();
+            state.decUnoccupiedRegisters();
+            new PayEvent(state,queue,state.getQueueTime()+state.getLastPayTime(), state.getFirstNumber());
+        }
+
+
+    }
 }
