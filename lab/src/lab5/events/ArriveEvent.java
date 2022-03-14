8f9334a5b43b477d77c2abe394d6372e3da2183d
himla-Lois <89983005+himla-Lois@users.noreply.github.com>
2022-03-14 23:32:24 +0100

    Update ArriveEvent.java

--- lab/src/lab5/events/ArriveEvent.java
+++ lab/src/lab5/events/ArriveEvent.java
@@ -1,41 +1,48 @@
-package lab5.events;
+package events;
 
-import lab5.state.FIFO;
-import lab5.state.CustomerFactory;
-import lab5.events.PickEvent;
-import lab5.state.State;
+import lab5.events.Event;
+import state.ShoppingState;
+import state.State;
 
 /**
  * Represents the event; arrival, of a customer.
  */
-public class ArriveEvent extends Event{
 
-    private FIFO f;
-    private CustomerFactory cf;
+public class ArriveEvent extends Event {
+    private double time;
+    private EventQueue eQueue;
 
-    ArriveEvent(State state, EventQueue queue, double time){
-        super(state, queue, time);
+    ArriveEvent(State state, EventQueue eQueue, double time) {
+        super(state, eQueue, time);
+
     }
 
-    /**
-     * Carries out the effect of a customer arrival at the supermarket.
-     */
-    public void effect() throws IllegalArgumentException {
+    @Override
+    public void effect(State state) {
+        ShoppingState shoppingState = (ShoppingState) state; // Turn shoppingstate to state.
 
-
-
-        if (f.isFull()) { // TODO: should also check if store id open
-            throw new IllegalArgumentException("The supermarket is full.");
-            // TODO: should add a missed customer if the store was open but full.
+        if (shoppingState.getCurrentTime() < shoppingState.S) { // see if the store is still before closingtime.
+            shoppingState.updateQAndRLogTime(time);
         }
-        // TODO: should check if store is full, and closed. If closed, return false or
-        //  throw exception.
-        cf.createCustomer();
 
-        // TODO:
-        //  (1) a pickEvent should be created here when customer is done picking groceries.
-        //  (2) lastly, this effect should create a new future arrive event for the next customer.
+        effect(state);
 
+        shoppingState.makeCustomer();
 
+        double arrivalTime = shoppingState.getCurrentTime() + shoppingState.nextArrivalRandom(); // Time where customer
+                                                                                                 // arrived
+        ArriveEvent arrivalEvent = new ArriveEvent(state, eQueue, arrivalTime); // create arriveEvent here
+        eQueue.addFirst(arrivalEvent); // Added it to EventQueue
+
+        if (shoppingState.getNrOfCustomers() < shoppingState.M) { // looks if it's full with customers in the shop
+            shoppingState.increaseCustomers();
+
+        } else { // om det är fullt i "butiken" så räknas kunden som förlorad
+            shoppingState.increaseLostCustomer();
+        }
+    }
+
+    protected String EventName() {
+        return "ArrivalEvent";
     }
 }
