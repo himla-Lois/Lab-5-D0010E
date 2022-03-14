579d8606c5f118be9b9f2f3bad8df61c4a2b3e64
strandafredde <75703920+strandafredde@users.noreply.github.com>
2022-03-14 13:33:27 +0100

    Update shoppingView.java

--- lab/src/lab5/view/shoppingView.java
+++ lab/src/lab5/view/shoppingView.java
@@ -3,6 +3,7 @@ package lab5.view;
 import lab5.events.ClosingEvent;
 import lab5.events.StopEvent;
 import lab5.state.ShoppingState;
+import lab5.state.State;
 import java.util.Observable;
 
 
@@ -12,6 +13,8 @@ import java.util.Observable;
  */
 public class shoppingView extends View {
 	private ShoppingState state;
+	private State stat;
+
 	/**
 	 * @param state 
 	 *  The state is used to accessed the variables from shoppingState
@@ -22,12 +25,12 @@ public class shoppingView extends View {
 	public void startParameters(){
 		System.out.println("PARAMETRAR");
 		System.out.println("==========");
-		System.out.println("Antal kassor, N " + state.N );
-		System.out.println("Max som ryms, M " + state.M);
-		System.out.println("Ankomsthastigheten lamda ");
-		System.out.println("Plocktider, [P_min..Pmax] ");
-		System.out.println("Betaltider, [K_min..Kmax] ");
-		System.out.println("Frö, f " + state.F);
+		System.out.println("Antal kassor, N..........: " + state.getN());
+		System.out.println("Max som ryms, M..........: " + state.getM());
+		System.out.println("Ankomsthastigheten, lamda: " + state.getLambda());
+		System.out.println("Plocktider, [P_min..Pmax]: " + "[" + state.getPickMin() + ".."+ state.getPickMax() );
+		System.out.println("Betaltider, [K_min..Kmax]: " + "[" + state.getPayMin() + ".."+ state.getPayMax());
+		System.out.println("Frö, f...................: " + state.getF());
 		System.out.println("Tid  Händelse  Kund  ?  led  ledT  I  $  :-(  köat  köT  köar  [Kassakö..]");
 	}
 	/**
@@ -35,34 +38,35 @@ public class shoppingView extends View {
 	 */
 	public void printRow(Object arg1) {
 		if (arg1 instanceof ClosingEvent) {
-			System.out.println("0 " + " " + HÄNDELSENAMN);
+			System.out.println("0 " + " " + "closing");
 		}
 		
 		if (arg1 instanceof StopEvent) {
-			System.out.println(TID + " " + HÄNDELSENAMN);
+			System.out.println(state.getS() + " " + "stop");
 		}
 		else {
-			System.out.println(TID + " " + arg1 + " " + customerid +" "+
-			Event + " " + LEDIGA KASSOR + " " + LEDIGA KASSOR TID + " "  + KUNDER JUST NU +
-			" " + state.payingCustomer + " " + state.lostCustomer + " " + ANTAL I KÖ + " " +
-			state.queueTime + " " + LÄNGD PÅ KÖN + " " + KASSAKÖ);
+			System.out.println(stat.getTime() + " " + arg1 + " " + state.getCustomerID() +" "+
+			state.getStatus() + " " + state.getUnoccupiedRegisters() + " " + state.wastedCashierTime() + " "  + state.getQueuedCustomers() +
+			" " + state.getPayingCustomers() + " " + state.getLostCustomers() + " " + state.getNrOfCustomers() + " " +
+			state.getQueueTime() + " " + state.getNrOfCustomers() + " " + state.getQueueString());
 		}
 		
 
 	}
 	
 	public void printResult() {
-		System.out.println("1) Av " + (state.payingCustomer + state.lostCustomer) + 
-				"kunder handlade " + state.payingCustomer + "medan " + state.lostCustomer +
+		System.out.println("1) Av " + (state.getPayingCustomers ()+ state.getLostCustomers()) + 
+				"kunder handlade " + state.getPayingCustomers() + "medan " + state.getLostCustomers() +
 				"missade.");
 		
-		System.out.println("2) Total tid " + ANTAL LEDIGA ASSOR +" kassor varit lediga:" +
-				TID KASSA LEDIG);
-		System.out.println("Genomsnittlig ledig kassatid: " + (TID KASSA LEDIG / ANTAL LEDIGA KASSOR) +
-				"(dvs" + TID + "av tiden från öppning tills sista kunden betalat).");
+		System.out.println("2) Total tid " + state.getUnoccupiedRegisters() +" kassor varit lediga:" +
+				state.wastedCashierTime());
+		System.out.println("Genomsnittlig ledig kassatid: " + (state.wastedCashierTime()  / state.getUnoccupiedRegisters()) +
+				"(dvs" + state.getLastPayTime() + "av tiden från öppning tills sista kunden betalat).");
 				
-		System.out.println("3) Total tid " + ANTAL I KÖ + "kunder tvingats köa: " +
-				state.queueTime +);
-		System.out.println("Genomsnittlig kötid: " + state.queueTime / ANTAL I KÖ );
+		System.out.println("3) Total tid " + state.getQueuedCustomers() + "kunder tvingats köa: " +
+				state.getQueueTime() + "te");
+		System.out.println("Genomsnittlig kötid: " + state.getQueueTime() / state.getQueuedCustomers());
 	}
 }
+
